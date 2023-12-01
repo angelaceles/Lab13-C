1146560d42b03f0de2128bcae93785d8166c872b
Alumno <Alumno@LAB1504-14>
2023-11-30 21:54:15 -0500

    Agregar archivos de proyecto.

---
+++ Lab13-C/Controllers/CustomersPController.cs
@@ -0,0 +1,26 @@
+﻿using Lab13_C.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab13_C.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersPController : ControllerBase
+    {
+        private readonly InvoiceContext _context;
+        public CustomersPController(InvoiceContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async List<Customer> GetCustomersP() 
+        {
+            if (_context.Customers == null)
+            {
+                return NotFound();
+            }
+            return await _context.Customers.ToListAsync();
+        }
+    }
+}
